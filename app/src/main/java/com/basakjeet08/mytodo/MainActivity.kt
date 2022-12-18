08d186bbf231c82abb4fb77fbabbd308cbafa433
Anirban Basak <jeetbasak2002@gmail.com>
2022-12-19 04:28:14 +0530

    Made the Home Page

--- app/src/main/java/com/basakjeet08/mytodo/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/mytodo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.basakjeet08.mytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.basakjeet08.mytodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
